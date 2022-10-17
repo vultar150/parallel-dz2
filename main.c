9e0b4b8a5f5482d78a082ba6170467f1fcd60787
vultar150 <vultar1500@gmail.com>
2022-10-17 17:24:19 +0300

    comment: added functions documentation

--- main.c
+++ main.c
@@ -9,13 +9,108 @@
     #define M_PI 3.14159265358979323846
 #endif
 
-const unsigned NUM_OF_POINTS = 1024;
-int myrank, ranksize; // current rank and number of processes in communicator
+const unsigned NUM_OF_POINTS = 1024; // Number of points to be generated in one iteration
+int myrank; // Current rank
+int ranksize; // Number of processes in communicator
 
 
 typedef struct {
     double x, y, z;
 } point_t;
+
+/**
+ * @param min lower bound
+ * @param max upper bound
+ * @return random real value from min to max
+ */
+double uniformDist(double min, double max);
+
+/**
+ * @brief The function generates n points 
+ * within a parallelepiped П
+ * 
+ * @param points the array where the generated 
+ * points will be stored
+ * @param n number of generated points
+ */
+void pointsGenerator(point_t *points, unsigned n);
+
+/**
+ * @brief Subintegral function
+ * 
+ * @param p function argument
+ * @return function value
+ */
+double func(point_t *p);
+
+/**
+ * @brief Function that determines 
+ * whether a point p belongs to the 
+ * integration area G
+ * 
+ * @param p point
+ * @return 0 or 1
+ */
+uint8_t insideArea(point_t *p);
+
+/**
+ * @brief Initializing sendcounts and displs 
+ * parameters of the MPI_Scatterv function
+ * 
+ * @param sendcounts an array of values of the number 
+ * of elements to be passed to the processes
+ * @param displs array of offset values
+ */
+void initScattervParams(int *sendcounts, int *displs);
+
+/**
+ * @brief Numerical Monte Carlo method 
+ * for calculating the value of a definite integral.
+ * 
+ * @param f subintegral function
+ * @param inside function that determines 
+ * whether a point belongs to the integration area G 
+ * @param pointsGen points generator
+ * @param vol the volume of the parallelepiped П
+ * which completely contains the integration area G
+ * @param analytical_res value of the integral 
+ * calculated analytically
+ * @param eps calculation accuracy
+ */
+void MonteCarloParallel(double (*f)(point_t *),
+                        uint8_t (*inside)(point_t *),
+                        void (*pointsGen)(point_t *, unsigned),
+                        const double vol,
+                        const double analytical_res,
+                        const double eps);
+
+
+int main(int argc, char **argv)
+{
+
+    int error;
+    error = MPI_Init(&argc, &argv);
+    if (error != MPI_SUCCESS) {
+        fprintf(stderr, "ERROR: CAN'T MPI INIT\n");
+        return 1;
+    }
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Comm_size(MPI_COMM_WORLD, &ranksize);
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+    const double analytical_res = 4.0 * M_PI / 3.0; 
+    const double volume = 8;
+    double eps = 1.0e-4; // default eps value
+
+    if (argc >= 2) sscanf(argv[1], "%lf", &eps);  
+
+    MonteCarloParallel(func, insideArea, pointsGenerator, 
+                       volume, analytical_res, eps);
+
+    MPI_Finalize();
+    return 0;
+}
 
 
 double uniformDist(double min, double max)
@@ -47,14 +142,6 @@ uint8_t insideArea(point_t *p)
 }
 
 
-/**
- * @brief Initializing sendcounts and displs 
- * parameters of the MPI_Scatterv function
- * 
- * @param sendcounts an array of values of the number 
- * of elements to be passed to the processes
- * @param displs array of offset values
- */
 void initScattervParams(int *sendcounts, int *displs)
 {
     int nmin = (NUM_OF_POINTS / (ranksize - 1)) * 3;
@@ -70,15 +157,7 @@ void initScattervParams(int *sendcounts, int *displs)
     }
 }
 
-/**
- * @brief 
- * 
- * @param f 
- * @param inside 
- * @param vol 
- * @param analytical_res 
- * @param eps 
- */
+
 void MonteCarloParallel(double (*f)(point_t *),
                         uint8_t (*inside)(point_t *),
                         void (*pointsGen)(point_t *, unsigned),
@@ -160,31 +239,4 @@ void MonteCarloParallel(double (*f)(point_t *),
         free(subRandPoints);
     }
     MPI_Barrier(MPI_COMM_WORLD);
-}
-
-int main(int argc, char **argv)
-{
-
-    int error;
-    error = MPI_Init(&argc, &argv);
-    if (error != MPI_SUCCESS) {
-        fprintf(stderr, "ERROR: CAN'T MPI INIT\n");
-        return 1;
-    }
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    MPI_Comm_size(MPI_COMM_WORLD, &ranksize);
-    MPI_Barrier(MPI_COMM_WORLD);
-    
-    const double analytical_res = 4.0 * M_PI / 3.0; 
-    const double volume = 8;
-    double eps = 1.0e-4; // default eps value
-
-    if (argc >= 2) sscanf(argv[1], "%lf", &eps);  
-
-    MonteCarloParallel(func, insideArea, pointsGenerator, 
-                       volume, analytical_res, eps);
-
-    MPI_Finalize();
-    return 0;
 }
