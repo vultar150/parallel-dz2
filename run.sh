508bc768c0009a6ac6523d09273dd018a394a896
Рябченков Владимир Михайлович <edu-cmc-skmodel22-621-04@polus-ib.bmc.hpc.cs.msu.ru>
2022-10-17 18:34:52 +0300

    feat: added run script (for Polus)

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+for ((i = 2; i <= 32; i*=2))
+do
+for eps in 3.0e-6 5.0e-7 1.5e-7
+do
+mpisubmit.pl -p $i -w 00:15 --stdout output/mainout$i-$eps.out --stderr err/mainerr$i-$eps.err a.out -- $eps
+done
+done
