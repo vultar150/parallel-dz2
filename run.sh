02e92bc8de8aaec8b7582cabe8d7565d7a957f84
vultar150 <vultar1500@gmail.com>
2022-10-22 21:41:26 +0300

    fix: changed eps and number of processes

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 mkdir -p output 
 mkdir -p err
 
-for ((i = 2; i <= 32; i*=2))
+for i in 2 4 8 16 60
 do
-for eps in 3.0e-6 5.0e-7 1.5e-7
+for eps in 3.0e-5 5.0e-6 1.5e-6
 do
 mpisubmit.pl -p $i -w 00:15 --stdout output/mainout$i-$eps.out --stderr err/mainerr$i-$eps.err a.out -- $eps
 done
